16b6b029601f31b77a1d3fe8bd78ec76be1d7927
alifmuqri.hazm <alifmuqri.hazm@petronas.com>
2024-11-18 17:03:03 +0800

    create global logger with performance, usage, error and diagnostic log events

---
+++ Flogging.Core/Flogger.cs
@@ -0,0 +1,63 @@
+﻿using Serilog;
+using Serilog.Events;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flogging.Core
+{
+    static public class Flogger
+    {
+        private static readonly ILogger _perfLogger;
+        private static readonly ILogger _usageLogger;
+        private static readonly ILogger _errorLogger;
+        private static readonly ILogger _diagnosticLogger;
+
+        static Flogger()
+        {
+            _perfLogger = new LoggerConfiguration()
+                .WriteTo.File(path: "C:\\Users\\alifmuqri.hazm\\source\\repos\\FloggingPS\\Flogging.Core\\Logs\\perf.txt")
+                .CreateLogger();
+
+            _usageLogger = new LoggerConfiguration()
+                .WriteTo.File(path: "C:\\Users\\alifmuqri.hazm\\source\\repos\\FloggingPS\\Flogging.Core\\Logs\\usage.txt")
+                .CreateLogger();
+
+            _errorLogger = new LoggerConfiguration()
+                .WriteTo.File(path: "C:\\Users\\alifmuqri.hazm\\source\\repos\\FloggingPS\\Flogging.Core\\Logs\\error.txt")
+                .CreateLogger();
+
+            _diagnosticLogger = new LoggerConfiguration()
+                .WriteTo.File(path: "C:\\Users\\alifmuqri.hazm\\source\\repos\\FloggingPS\\Flogging.Core\\Logs\\diagnostic.txt")
+                .CreateLogger();
+
+        }
+
+        public static void WritePerf(FlogDetail infoToLog)
+        {
+            _perfLogger.Write(LogEventLevel.Information, "{@FlogDetail}", infoToLog);
+        }
+
+        public static void WriteUsage(FlogDetail infoToLog)
+        {
+            _usageLogger.Write(LogEventLevel.Information, "{@FlogDetail}", infoToLog);
+        }
+
+        public static void WriteError(FlogDetail infoToLog)
+        {
+            _errorLogger.Write(LogEventLevel.Information, "{@FlogDetail}", infoToLog);
+        }
+
+        public static void WriteDiagnostic(FlogDetail infoToLog)
+        {
+            var writeDiagnostics = Convert.ToBoolean(ConfigurationManager.AppSettings["EnableDiagnostic"]);
+            if (!writeDiagnostics)
+                return;
+
+            _diagnosticLogger.Write(LogEventLevel.Information, "{@FlogDetail}", infoToLog);
+        }
+    }
+}
